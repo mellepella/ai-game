53ddd4b3f008dc11efdb9c7c47c4c92f68a95eec
Melvin Wahlberg <mellepella@gmail.com>
2022-08-23 19:13:25 +0200

    Add GameObstacles file

---
+++ scripts/GameObstacles.js
@@ -0,0 +1,18 @@
+const GameObstacles = [
+  new Obstacle(
+    { x: unitsToPx(8), y: unitsToPx(13) },
+    { width: unitsToPx(2), height: unitsToPx(13) }
+  ),
+  new Obstacle(
+    { x: unitsToPx(27), y: unitsToPx(13) },
+    { width: unitsToPx(2), height: unitsToPx(12) }
+  ),
+  new Obstacle(
+    { x: unitsToPx(13), y: unitsToPx(0) },
+    { width: unitsToPx(2), height: unitsToPx(13) }
+  ),
+  new Obstacle(
+    { x: unitsToPx(32), y: unitsToPx(0) },
+    { width: unitsToPx(2), height: unitsToPx(13) }
+  ),
+];
